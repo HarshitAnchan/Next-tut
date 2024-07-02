53762363b62e421389ba666055f3340829ab7298
harshitanchan <harshitanchan647@gmail.com>
2024-07-02 21:13:21 +0530

    active links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
+import Link from "next/link"
+
+
 export default function Home(){
     return(
-        <h1>Home Page</h1>
+        <>
+                <h1>Home Page</h1>
+                <Link href="/blog">Blog</Link>
+                <Link href="/products">Products</Link>
+
+        </>
     )
 }

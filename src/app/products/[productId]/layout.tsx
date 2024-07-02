e940020fef82d0a9a260602c94c69320963cab20
harshitanchan <harshitanchan647@gmail.com>
2024-07-02 11:54:52 +0530

    Route Group Layout

---
+++ src/app/products/[productId]/layout.tsx
@@ -0,0 +1,16 @@
+export default function ProductDetailsLayout( {
+    children,
+}: {
+    children: React.ReactNode;
+}){
+    return(
+        <>
+        {children}
+        <h1>
+          Features products
+      </h1>
+        </>
+      
+    )
+  }
+  
